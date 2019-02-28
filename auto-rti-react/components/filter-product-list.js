85e1370076fc3a9542a6794dc31a6de98f7e9460
Volodymyr Frolov <volodymyr@yaelconsulting.com>
2019-02-28 19:47:19 +0200

    created product list

---
+++ auto-rti-react/components/filter-product-list.js
@@ -0,0 +1,16 @@
+export default class extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        Категория
+                    </div>
+                    <div className="col-sm-6">
+                        Машина
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- auto-rti-react/components/layout.js
+++ auto-rti-react/components/layout.js
@@ -1,21 +1,26 @@
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import ProductList from './product-list'
 
 
 
 export default class extends React.Component {
-    state = { dataListPage: null }
+    state = {
+        dataListPage: null,
+    }
 
     componentDidMount() {
         fetch(`http://127.0.0.1:9000/api/auto-rti/`)
             .then((resListPage) => resListPage.json())
             .then((dataListPage) => {
                 this.setState({ dataListPage })
-            })
+            })        
     }
     
     render() {
+        console.log('@@@@', this.state.dataListPage)
         let menu = null
         if (this.state.dataListPage !== null) {
             menu = this.state.dataListPage.results.map(({id, slug, title}) => (
@@ -24,13 +29,13 @@ export default class extends React.Component {
                         <a>{title}</a>
                     </Link>
                     <style jsx>{`
-                    .menu{
-                        background: #0000aa;
-                        border: #000000 1px dashed;
-                        padding: 4px;
-                    }
-                `}
-                </style>
+                        .menu{
+                            background: #0000aa;
+                            padding: 2px;
+                            margin: 6px;
+                        }                       
+                    `}
+                    </style>
                 </li>
             ))
         }
@@ -39,22 +44,28 @@ export default class extends React.Component {
                 <title>Авторезинотехника РТИ Лоск Харьков Украина резинотехнические изделия сальники ремкомплекты автозапчасти втулки манжеты авто сельхозтехника ремни клиновые опт</title>
                 
                 <div className="container">
-                    <img src="/static/images/bg_top.jpg"></img>
+                    <div className="row">
+                        <img src="/static/images/bg_top.jpg"></img>
+                    </div>
                     <div className="row td_mid">
-                        <div className="col-sm-5">
+                        <div className="col-sm-4">
                             <ul>{menu}</ul>
                         </div>
-                        <div className="col-sm-7">
+                        <div className="col-sm-8">
                             {this.props.children}
+                            {this.props.children._self.props.page !== 'products' ? null : <ProductList/>}
                         </div>
                     </div>
-                </div>                
+                </div> 
             
                 <style jsx>{`
                     .td_mid{
                     background-image: url('static/images/bg_mid.png');
-                    pading: 0px;
-                    padding-left: 10px
+                    pading: 10px;
+                    padding-left: 0px;
+                    padding-right: 5px;
+                    height:350px;
+                    width:768px;
                     }
                 `}
                 </style>
